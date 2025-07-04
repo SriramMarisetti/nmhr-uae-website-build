de3dcd5cbc96244174b3200d2e0a50a5e7fe30c3
Ch N V S S Durga Prasad <chodagiriprasad5@gmail.com>
2025-07-04 20:32:19 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+      <ScrollToTop/>
         <div className="min-h-screen flex flex-col">
           <Header />
           <main className="flex-1">
